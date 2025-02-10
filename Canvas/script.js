ec7dedd4b2b4c8f67788abc356157e7d1bdadb14
Brian J. Brun <brian.brun7@gmail.com>
2025-02-10 07:14:32 -0600

    Canvas Drawing circles

--- Canvas/script.js
+++ Canvas/script.js
@@ -4,51 +4,42 @@ let ctx = canvas.getContext("2d");
 
 let width = canvas.width;
 let height = canvas.height;
-let opacity = 1;
 
-let red = 0;
-let green = 0;
-let blue = 0;
+let x = 0;
+let y = height / 2;
+let forward = true;
 
 function drawCircle(x, y) {
-  ctx.fillStyle = `rgba(${red},${green},${blue},${opacity})`;
+  ctx.fillStyle = "rgb(0, 128,255)";
   ctx.beginPath();
   ctx.arc(x, y, 10, 0, Math.PI * 2, false);
   ctx.fill();
 }
 
-canvas.addEventListener("click", (e) => {
-  drawCircle(e.offsetX, e.offsetY);
-});
+function update() {
+  if(x===width) {
+    forward = false;
+  }
+  else if (x === 0) {
+    forward = true;
+  }
 
-let clearBtn = document.querySelector("#clear");
-clearBtn.addEventListener("click", () => {
-  ctx.clearRect(0, 0, 300, 300);
-});
+  if( forward ) {
+    x += 1;
+  }
+  else {
+    x -= 1;
+  }
+}
 
-let opacitySlider = document.querySelector("#opacity");
-opacitySlider.addEventListener("input", (e) => {
-  opacity = e.target.value;
-});
 
-let redValue = document.getElementById("redValue");
-let greenValue = document.getElementById("greenValue");
-let blueValue = document.getElementById("blueValue");
 
-let redSlider = document.querySelector("#red");
-redSlider.addEventListener("input", (e) => {
-  red = e.target.value;
-  redValue.textContent = red;
-});
+function draw() {
+  ctx.clearRect(0, 0, width, height);
+  drawCircle(x%width,y%height);
+}
 
-let greenSlider = document.querySelector("#green");
-greenSlider.addEventListener("input", (e) => {
-  green = e.target.value;
-  greenValue.textContent = green;
-});
-
-let blueSlider = document.querySelector("#blue");
-blueSlider.addEventListener("input", (e) => {
-  blue = e.target.value;
-  blueValue.textContent = blue;
-});
+setInterval( () => {
+  update();
+  draw();
+}, 10);
